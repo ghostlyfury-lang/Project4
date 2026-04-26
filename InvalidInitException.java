e3e4294f267800134f2954ded7c7f34210732fb3
LLRizzo <ghostlyfury@gmail.com>
2026-04-26 12:57:35 -0400

    Update InvalidInitException.java

--- InvalidInitException.java
+++ InvalidInitException.java
@@ -1,1 +1,23 @@
+/**
+ * Simple exception class to inform us when the things go wrong during
+ * initializition.
+ */
+public class InvalidInitException extends Exception {
 
+    /**
+     * Constructor that creates an exception object
+     * and takes a message as an argument
+     * @param message The message you want to output
+     */
+    public InvalidInitException(String message) {
+        super(message);
+    }
+    /**
+     * Default constructor with no arguments that
+     * created an exception object and outputs a
+     * default message
+     */
+    public InvalidInitException() {
+        super("Invalid maze initialization");
+    }
+}
