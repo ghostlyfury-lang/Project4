91e3a9c36fb96cd34970941b9aa247981528c698
JonathanSeda <jonathanaseda@gmail.com>
2026-05-04 17:38:50 -0400

    Update MazeCell.java
    Just in case

--- MazeCell.java
+++ MazeCell.java
@@ -2,10 +2,18 @@
  * Characters needed for drawing a Maze.
  */
 public enum MazeCell {
-    ENTER,
-    EXIT,
-    INVALID,
-    OPEN,
-    PERSON,
-    WALL;
+    ENTER("Enter"),
+    EXIT("Exit"),
+    INVALID("Invalid"),
+    OPEN("Open"),
+    PERSON("Person"),
+    WALL("Wall");
 
+    private String name;
+    private MazeCell(String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return name;
+    }
+}
