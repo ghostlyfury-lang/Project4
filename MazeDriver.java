d87aa9133e74008fac39b27e10c84b1eee8bfa3a
JonathanSeda <jonathanaseda@gmail.com>
2026-05-04 23:33:36 -0400

    Update MazeDriver.java

--- MazeDriver.java
+++ MazeDriver.java
@@ -1,11 +1,27 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ * Drives the process of constructing and solving a maze
+ * @author Luca Rizzo, Jonathan Seda
+ * @version May, 2026
+ */
 public class MazeDriver {
-    public MazeDriver() {}
-
+    /**
+     * The main method for the program of constructing and solving
+     * a maze
+     * @param args unused
+     */
     public static void main(String[] args) {
+        //try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is the name of the file with the maze?");
+            String fileName = "";
+            fileName = input.nextLine();
+            input.close();
+        //} catch ??
         MazeRunner newRunner = null;
         try {
-            newRunner = new MazeRunner("minimaze.txt");
+            newRunner = new MazeRunner(fileName);
             newRunner.runMaze();
         } catch (InvalidInitException e) {
             System.out.println(e.getMessage());
