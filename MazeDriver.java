f62f15a652ec8324ead469f1e4c26aa031fb56e3
JonathanSeda <jonathanaseda@gmail.com>
2026-05-04 20:33:58 -0400

    Update MazeDriver.java

--- MazeDriver.java
+++ MazeDriver.java
@@ -1,1 +1,16 @@
+import java.io.FileNotFoundException;
+public class MazeDriver {
+    public MazeDriver() {}
 
+    public static void main(String[] args) {
+        MazeRunner newRunner = null;
+        try {
+            newRunner = new MazeRunner("minimaze.txt");
+            newRunner.runMaze();
+        } catch (InvalidInitException e) {
+            System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
