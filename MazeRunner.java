735150e6eebd3182791e708a920e42f7764328e8
JonathanSeda <jonathanaseda@gmail.com>
2026-05-04 22:57:33 -0400

    Update MazeRunner.java

--- MazeRunner.java
+++ MazeRunner.java
@@ -7,6 +7,7 @@ public class MazeRunner{
     private Point entrance;
     private LinkedList<Stack<Point>> stackStorage;
     private int nodesChecked = 0;
+    private LinkedList<Point> finalPath = new LinkedList<Point>();
     public MazeRunner(String filename) throws InvalidInitException,
                     FileNotFoundException {
         layout = new Maze(filename);
@@ -26,16 +27,16 @@ public class MazeRunner{
             nodesChecked++;
         }
     }
-    private void createTravelables(Stack<Point> s) {
-                Point step = s.peek();
+        private void createTravelables(Stack<Point> s) {
+        Point step = s.peek();
         Point east = (Point) step.clone();
         Point north = (Point) step.clone();
         Point west = (Point) step.clone();
         Point south = (Point) step.clone();
         east.translate(1, 0);
-        north.translate(0, 1); //technically because of the way the
-        west.translate(-1, 0); //grid is set up, south is actually
-        south.translate(0, -1); //north and north is actually south
+        north.translate(0, 1);
+        west.translate(-1, 0);
+        south.translate(0, -1);
         storeIfPushable(s, east);
         storeIfPushable(s, north);
         storeIfPushable(s, west);
@@ -44,40 +45,31 @@ public class MazeRunner{
 
     public void runMaze() {
         boolean foundExit = false;
-        Stack<Point> exitPath = new Stack<Point>();
         Stack<Point> initialPoint = new Stack<Point>();
         initialPoint.push(entrance);
         stackStorage.push(initialPoint);
         nodesChecked++;
-        Stack<Point> debugStack = new Stack<Point>();
         while (!foundExit) {
             if (stackStorage.size() == 0) {
-                                System.out.print("Despite our best efforts, this");
+                System.out.print("Despite our best efforts, this");
                 System.out.println(" maze appears to be unsolvable.");
-                System.out.println(nodesChecked + " nodes checked:");
-                while (debugStack.size() != 0) {
-                    System.out.println(debugStack.pop().toString());
-                }
+                                System.out.println(nodesChecked + " points checked.");
                 System.exit(1);
             }
             Stack<Point> nextStack = stackStorage.remove();
-            debugStack = nextStack;
             if (layout.isExit(nextStack.peek())) {
                 foundExit = true;
-                exitPath = nextStack;
+                int iterations = nextStack.size();
+                for (int i = 0; i < iterations; i++) {
+                    Point p = nextStack.pop();
+                    finalPath.add(p);
+                }
             } else {
                 createTravelables(nextStack);
             }
         }
-        Stack<Point> reversal = new Stack<Point>();
-        for (int i = 0; i < exitPath.size(); i++) {
-            reversal.push(exitPath.pop());
-        }
-        String path = "";
-        for (int i = 0; i < reversal.size(); i++) {
-            Point p = reversal.pop();
-            path += p.toString();
-            path += "---";
-        }
+
+        layout.setPath(finalPath);
+        System.out.println(layout.toString());
     }
 }
