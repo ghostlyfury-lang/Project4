5845a441e1c70ddcd29be812efd752cc5eaadc18
JonathanSeda <jonathanaseda@gmail.com>
2026-05-04 23:33:00 -0400

    Update MazeCell.java

--- MazeCell.java
+++ MazeCell.java
@@ -1,19 +1,39 @@
 /**
  * Characters needed for drawing a Maze.
+ * @author Luca Rizzo, Jonathan Seda
+ * @version May, 2026
  */
 public enum MazeCell {
+    /** Entrance to the maze */
     ENTER("Enter"),
+    /** Exit of the maze */
     EXIT("Exit"),
+    /** Not a valid function within the maze */
     INVALID("Invalid"),
+    /** Walkway passage of the maze */
     OPEN("Open"),
+    /** Marks a path to an exit */
     PERSON("Person"),
+    /** Cannot be passed through in the maze */
     WALL("Wall");
 
-    private String name;
-    private MazeCell(String name) {
-        this.name = name;
+    /**
+     * The name or text associated with a MazeCell
+     */
+    private String text;
+
+    /**
+     * Default constructor for a MazeCell enum
+     * @param text the text associated with the enum
+     */
+    private MazeCell(String text) {
+                this.text = text;
     }
+    /**
+     * Converts the text of a MazeCell into a printable string
+     * @return the string form of the MazeCell
+     */
     public String toString() {
-        return name;
+        return text;
     }
 }
